4e43bef7c7b49cb60038be0697e076585cced11e
Pattrick Hueper <phueper@hueper.net>
2013-08-27 22:11:39 +0200

    add oracle-xe rpm package, run /etc/init.d/oracle_xe configure, add sqldeveloper rpm
    Signed-off-by: Pattrick Hueper <phueper@hueper.net>

--- cookbooks/oraxe/recipes/default.rb
+++ cookbooks/oraxe/recipes/default.rb
@@ -9,3 +9,39 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+#
+
+package "oracle-rdbms-server-11gR2-preinstall" do
+  action :install
+end
+
+rpm_package "oracle-xe" do
+  source "/vagrant/oraxe_install/Disk1/oracle-xe-*.rpm"
+  action :install
+end
+
+file "test" do
+  path "/tmp/patty_test"
+  content <<-EOF
+ORACLE_HTTP_PORT=8080
+ORACLE_LISTENER_PORT=1521
+ORACLE_PASSWORD=oracle
+ORACLE_CONFIRM_PASSWORD=oracle
+ORACLE_DBENABLE=y
+  EOF
+end
+
+execute "oracle_xe configure" do
+  command "/etc/init.d/oracle-xe configure responseFile=/tmp/patty_test 2>&1 | tee /tmp/patty_test.out"
+end
+
+file "test_delete" do
+  path "/tmp/patty_test"
+  action :delete
+end
+
+rpm_package "sqldeveloper" do
+  source "/vagrant/oraxe_install/sqldeveloper-*.rpm"
+  action :install
+end
+
