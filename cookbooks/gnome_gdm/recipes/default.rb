76437baa09984cc0e2ae9fb768c69e9f5d6f0ee0
Pattrick Hueper <phueper@hueper.net>
2013-09-05 22:27:56 +0200

    single packages dont seem to work, lets groupinstall x and gnome
    Signed-off-by: Pattrick Hueper <phueper@hueper.net>

--- cookbooks/gnome_gdm/recipes/default.rb
+++ cookbooks/gnome_gdm/recipes/default.rb
@@ -7,26 +7,36 @@
 # All rights reserved - Do Not Redistribute
 #
 
-package "gdm" do
-  action :install
+#package "gdm" do
+#  action :install
+#end
+
+#package "gnome-desktop" do
+#  action :install
+#end
+
+#package "dejavu-fonts-common" do
+#  action :install
+#end
+
+#package "dejavu-sans-fonts" do
+#  action :install
+#end
+
+#package "dejavu-sans-mono-fonts" do
+#  action :install
+#end
+
+#package "dejavu-serif-fonts" do
+#  action :install
+#end
+
+#individual packages are not really working, let's try groupinstall for now
+
+execute "groupinstall X" do
+  command "yum groupinstall -y \"X Window System\""
 end
 
-package "gnome-desktop" do
-  action :install
-end
-
-package "dejavu-fonts-common" do
-  action :install
-end
-
-package "dejavu-sans-fonts" do
-  action :install
-end
-
-package "dejavu-sans-mono-fonts" do
-  action :install
-end
-
-package "dejavu-serif-fonts" do
-  action :install
+execute "groupinstall gnome" do
+  command "yum groupinstall -y \"Desktop\""
 end
